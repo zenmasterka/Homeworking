c26199db7fd055822a9663096277b1ccf22b42ec
Сергей Вертинский <vertinski96@gmail.com>
2020-05-05 14:23:08 +0300

    Homework 4: практика, logical/if/else

---
+++ 24.04.20/Segey_Vertinskiy_HW4.py
@@ -0,0 +1,20 @@
+a = int(input('a:'))
+b = int(input('b:'))
+c = int(input('c:'))
+# #1) Если нет ни одного 0, вывести "Нет нулевых значений"
+# x1 = bool(a) and bool(b) and bool(c) and "Нет нулевых значений!!!"
+# print(x1)
+# #2) Вывести первое нулевое значение. Если все нули, то "Введены все нули!"
+# x2 = a or b or c or "Введены все нули!"
+# print(x2)
+#3-4)
+# if a > (b + c):
+#     print(a-b-c)
+# elif a < (b+c):
+#     print(b+c-a)
+# if a > 50 and (b > a or c > a):
+    # print("Вася")
+if a > 5 or (b==7 and c==7):
+    print("Петя")
+
+    
--- test/data_input.py
+++ test/data_input.py
@@ -23,12 +23,12 @@ pass
 # Height 200
 
 # 3) Списки(List)
-# print("Weight", type(sys.argv))#тип лист
-# print("a list", sys.argv)#вывод самого списка
-# print('80' in sys.argv)#наличие '80' в списке
-# print('80' not in sys.argv)
-#len(), min(), max() and etc..
-# print(sys.argv.index('80'))
+# # print("Weight", type(sys.argv))#тип лист
+# # print("a list", sys.argv)#вывод самого списка
+# # print('80' in sys.argv)#наличие '80' в списке
+# # print('80' not in sys.argv)
+# # len(), min(), max() and etc..
+# # print(sys.argv.index('80'))
 
 # a_string = 'Z cnhjrf, fdcjdc, dccjd/'
 # list = ['aa', 'bb', 'cc', 672364, [1, 2, 3, 'ty']]
@@ -46,12 +46,12 @@ pass
 
 # del a_list
 # print(a_list)
-#4) Кортежи(tuple)
+# #4) Кортежи(tuple)
 # t = (1,)
 # print(type(t))
-# a, b, c = ('s', 'd', 1)
+# a, b, c = ('s','d',1)
 
-# print(a,b,c)
+# print(a,"\n",b,"\n", c)
 
 # # 5) Словари - похожие на списки. Хранят ключ:значение. Ключ - неименияемый тип данных(int, float, string, tuple).
 # d =dict(
@@ -62,9 +62,55 @@ pass
 # print(str(d['света']) + ' y.o.')
 
 
-#6) Множество(set)
+# 6) Множество(set)
 s = set()
 print(type(s))
 s.add(1)
 s.add('www')
 print(s)
+
+
+#7)Операторы сравнения. Булевый тип.
+# False 
+# a = True
+# print(type(a))
+# print(True == True)
+# b = 5
+# c = 5
+# print(a == c)
+
+# a = 'dd'
+# b = 'asdasddd'
+# # print(a not in b)
+
+# print(2 > 3 and 100 == 100)
+# print(5 < 3 or 100 == 100)
+# #AND(*-умножение) ИМЕЕТ ВЫЩЕ ПРИОРИТЕТ, ЧЕМ OR(+ - сложение)! NOT - обращает значение переменной на противоположное.
+# toggler = False
+# #cicle
+# toggler = not toggler 
+
+# z = 0
+# a = z or 'Anon' or 'vasia'
+# print(a)
+
+# result = i or "WER"
+# # print(result)
+# i = int(input('Введите число: '))
+# if i < 100 :
+#     print('+++++++++++++')
+# else:
+#     print('--------------')
+
+# #CASE аналог в Python
+# a = {1: 'ssss', 2:'ss'}
+# a.get(1, 'nfrjuj pyfxtybz ytn')
+# a.get(3, 'sdsfsdgsdfdfg')
+# print(a)
+
+# i = 0 #
+# while i <= 10:
+#     print(i)
+#     i +=1
+
+
